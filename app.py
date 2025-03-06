d305b8ea46877d80e4bc77473daf76e3214334c5
moyan <1561515308@qq.com>
2025-03-06 22:59:46 +0800

    chore: 删除无用代码并更新依赖
    - 删除 main.py、requirements.txt 和 utils.py 中的无用代码
    - 更新 pyproject.toml 中的项目依赖

--- app.py
+++ app.py
@@ -175,4 +175,4 @@ async def list_file():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
