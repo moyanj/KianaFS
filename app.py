acc31d631c2e09a381c814e78f8e03d651e8d5ba
moyan <1561515308@qq.com>
2025-03-11 21:31:36 +0800

    feat(app): 添加跨域资源共享 (CORS) 中间件
    - 在 app.py 文件中引入了 CORSMiddleware
    - 为应用程序添加了全局的 CORS 配置，允许所有来源的跨域请求
    - 配置了允许所有方法和所有头部的跨域请求

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 import db
@@ -23,6 +24,15 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, title="KianaFS API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(StarletteHTTPException)
