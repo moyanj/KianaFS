93d7562590ce9b2e54c9315e52e271ffc52154d5
moyan <1561515308@qq.com>
2025-03-12 23:26:27 +0800

    feat(webui): 移除示例代码并添加 ElementPlus 依赖
    - 移除了 App.vue 和其他组件中的示例代码
    - 删除了 base.css、main.css 和 logo.svg 文件
    - 移除了路由配置中的示例路由
    - 添加了 ElementPlus 依赖

--- webui/src/main.ts
+++ webui/src/main.ts
@@ -1,8 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus';
 
 const app = createApp(App)
 
