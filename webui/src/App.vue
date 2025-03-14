4dab285c6f184bcb8efee9cabfb4babb1fc9550c
moyan <1561515308@qq.com>
2025-03-14 19:36:41 +0800

    feat(webui): 重构前端页面并添加新功能
    - 添加了概览、存储管理、文件管理、用户管理和设置等新页面
    - 集成了 Iconify 图标库，增加了页面图标
    - 优化了路由配置和页面跳转逻辑

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,36 +1,51 @@
 <script setup lang="ts">
-import { RouterLink, RouterView, useRouter } from 'vue-router';
+import { RouterView, useRouter } from 'vue-router';
 import { ElMenu, ElMenuItem } from 'element-plus';
 import { ref, watch } from "vue";
+import TablerDashboard from '~icons/tabler/dashboard';
+import TablerFile from '~icons/tabler/file';
+import TablerDatabase from '~icons/tabler/database';
+import TablerUser from '~icons/tabler/user';
+import TablerSettings from '~icons/tabler/settings'
 
 // 定义 active 引用的类型
-const active = ref<string>("");
+const active = ref("");
 
 // 使用 useRouter 钩子
 const router = useRouter();
-console.log(import.meta.env.BASE_URL)
 // 监听路由变化以更新 active 引用
 watch(
     () => router.currentRoute.value.path,
     (newPath) => {
-        active.value = "/" + newPath.split("/")[1];
-        console.log(import.meta.env.BASE_URL)
+        const baseUrl = import.meta.env.BASE_URL;
+        active.value = newPath.startsWith(baseUrl) ? newPath.slice(baseUrl.length) : newPath;
+        console.log(active.value);
     },
     { immediate: true } // 立即执行一次以设置初始值
 );
 </script>
 
 <template>
-    <el-menu class="header-menu" :class="{ 'hidden': active === '/login' }">
+    <el-menu class="header-menu" :class="{ 'hidden': active === '/login' }" :route="true" :default-active="active">
         <el-menu-item class="header-menu-title">
             <span>KianaFS</span>
         </el-menu-item>
-        <el-menu-item index="/">
-            <router-link to="/">Home</router-link>
+        <el-menu-item index="/" @click="router.push('/')">
+            <TablerDashboard />&nbsp;概览
         </el-menu-item>
-        <el-menu-item index="/about">
-            <router-link to="/about">About</router-link>
+        <el-menu-item index="/storages" @click="router.push('/storages')">
+            <TablerDatabase />&nbsp;存储管理
         </el-menu-item>
+        <el-menu-item index="/files" @click="router.push('/files')">
+            <TablerFile />&nbsp;文件管理
+        </el-menu-item>
+        <el-menu-item index="/users" @click="router.push('/users')">
+            <TablerUser />&nbsp;用户管理
+        </el-menu-item>
+        <el-menu-item index="/settings" @click="router.push('/settings')">
+            <TablerSettings />&nbsp;设置
+        </el-menu-item>
+
     </el-menu>
 
     <RouterView />
@@ -52,6 +67,12 @@ watch(
 
 .hidden {
     display: none;
+}
+
+.el-card {
+    width: 100%;
+    height: 100%;
+    --el-card-padding: 0;
 }
 </style>
 
--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Icons from "unplugin-icons/vite";
+import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +15,10 @@ export default defineConfig({
     Icons({
       compiler: 'vue3',
       autoInstall: true,
-    })
+    }),
+    ElementPlus({
+      // options
+    }),
   ],
   resolve: {
     alias: {
