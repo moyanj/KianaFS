3eddaf7f58584241861af4ef74e072d6a721ba55
moyan <1561515308@qq.com>
2025-03-15 15:18:39 +0800

    feat(webui): 重构登录页面并添加一言功能
    - 重置了 LoginView.vue 的布局和样式，使其更加美观和响应式
    - 添加了一言功能，用户可以在登录页面查看和刷新一言
    - 优化了登录表单的样式，增加了标题和更好的间距
    - 调整了按钮样式和位置，使其更加突出
    - 在 main.css 中添加了 body 样式，去除了默认的 margin

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -13,9 +13,18 @@ const form = ref({
     password: '',
     raw: true
 });
+const hitokoto = ref("")
 
 if (cookies.get('token')) {
     router.push('/');
+}
+
+function getHitokoto() {
+    fetch('https://v1.hitokoto.cn')
+        .then(response => response.json())
+        .then(data => {
+            hitokoto.value = data.hitokoto + " -- " + data.from
+        })
 }
 
 async function submitForm() {
@@ -29,24 +38,30 @@ async function submitForm() {
     })
         .then(response => response.json())
         .then(data => {
-            // 设置cookie
-            cookies.set('token', data.data, "7d");
-            router.push('/');
+            if (data.success) {
+                // 设置cookie
+                cookies.set('token', data.data, "7d");
+                router.push('/');
+            } else {
+                alert(data.msg);
+            }
         })
         .catch(error => {
             alert(error.msg);
         })
 }
 
+getHitokoto()
 
 </script>
 
 <template>
     <div class="co">
         <el-card class="container">
-            <ElForm id="loginForm">
-                <h1>KianaFS</h1>
-
+            <ElForm id="loginForm" size="large">
+                <div class="title">
+                    <h1>KianaFS</h1>
+                </div>
                 <ElFormItem label="用户" prop="username">
                     <ElInput v-model="form.username" placeholder="请输入用户名" />
 
@@ -54,51 +69,76 @@ async function submitForm() {
 
                 <ElFormItem label="密码" prop="password">
                     <ElInput type="password" v-model="form.password" placeholder="请输入密码" />
-
                 </ElFormItem>
 
                 <ElButton type="primary" class="good-button" @click="submitForm">登录</ElButton>
+                <br>
+                <div @click="getHitokoto" class="hitokoto">
+                    <p>{{ hitokoto }}</p>
+                </div>
             </ElForm>
         </el-card>
     </div>
 </template>
 
 <style scoped>
+.title {
+    margin-bottom: 10vh;
+    text-align: center;
+}
+
+.hitokoto {
+    margin-top: 17vh;
+}
+
 /* 设置整个页面的居中对齐 */
 .co {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
+    justify-content: left;
+    align-items: left;
     /* 添加这一行以确保容器占满整个视口高度 */
     width: 100vw;
+    height: 100vh;
+    background-size: cover;
+}
+
+.dark .co {
+    background-image: url('@/assets/login_dark.jpg');
+}
+
+.light .co {
+    background-image: url('@/assets/login_light.jpg');
 }
 
 .container {
     justify-content: center;
     align-items: center;
-    width: 65%;
+    width: 25%;
+    height: calc(100% - 40px);
     border-radius: 10px;
+    max-width: 380px;
     box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
     text-align: center;
-    margin: 0 auto;
+    margin: 20px 10px;
 }
 
 
 .good-button {
+    margin-top: 6.5vh;
+
     height: 50px;
     width: 100%;
-    background: linear-gradient(135deg, #ff82af 0%, #ffdb8b 100%);
+    background: linear-gradient(145deg, #ff68a0 0%, #ffcd62 100%);
     color: white;
     font-weight: bold;
     border: none;
     border-radius: 5px;
     cursor: pointer;
-    transition: background 0.3s ease;
+    transition: background 1s ease;
 }
 
 .good-button:hover {
-    background: linear-gradient(135deg, #ff72a0 0%, #ffd17b 100%);
+    background: linear-gradient(75deg, #ff72a0 0%, #ffd17b 100%);
     box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
 }
 
